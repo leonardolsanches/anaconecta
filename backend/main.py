1974c40519bd3155fc7e4bd109ac2bcb1c37ebde
Leonardo Sanches <leonardo.l.sanches@gmail.com>
2025-05-19 18:43:18 -0300

    mvp

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 import json
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../frontend", template_folder="../frontend")
 CORS(app)
 
 @app.route('/api/iniciativas', methods=['GET'])
@@ -12,5 +13,11 @@ def get_iniciativas():
         data = json.load(f)
     return jsonify(data['iniciativas'])
 
+@app.route('/')
+@app.route('/<path:path>')
+def serve_frontend(path="index.html"):
+    return send_from_directory(app.static_folder, path)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
